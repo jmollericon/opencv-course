b3016fe1936fd2ecaa1f1a891ba85f203f755065
Jorge Mollericon <jlmollericon@gmail.com>
2021-04-01 21:44:56 -0400

    :zap: Read videos

--- read.py
+++ read.py
@@ -1,11 +1,21 @@
 import cv2 as cv
 
-# read image
+# reading image
 img = cv.imread('resources/photos/cat.jpg')
 cv.imshow('Cat', img)
 
-# read large image
+# reading large image
 img2 = cv.imread('resources/photos/cat_large.jpg')
 cv.imshow('Cat (large image)', img2)
 
-cv.waitKey(0)
+#reading videos
+capture = cv.VideoCapture('resources/videos/dog.mp4')
+while True:
+  isTru, frame = capture.read()
+  cv.imshow('Video', frame)
+
+  if cv.waitKey(20) & 0xFF==ord('d'):
+    break
+
+capture.release()
+cv.destroyAllWindows()
