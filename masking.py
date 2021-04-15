8269de1600d3a9b1d378307faf60bd4385349107
Jorge Mollericon <jlmollericon@gmail.com>
2021-04-15 13:56:34 -0400

    :sparkles: Implement Masking

---
+++ masking.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('resources/photos/cats.jpg')
+cv.imshow('Cats', img)
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+cv.imshow('Blank image', blank)
+
+mask = cv.circle(blank.copy(), (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
+cv.imshow('Mask', mask)
+
+masked = cv.bitwise_and(img, img, mask=mask)
+cv.imshow('Masked image', masked)
+
+circle = mask
+rectangle = cv.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1)
+
+weird_shape = cv.bitwise_and(circle, rectangle)
+cv.imshow('weird shape', weird_shape)
+masked = cv.bitwise_and(img, img, mask=weird_shape)
+cv.imshow('Weird shape masked image', masked)
+
+cv.waitKey(0)
